48e1f6d07a59ff62198e7c42f41729659a6ff3c5
Paolo Lucano <paololucanodeveloper@gmail.com>
2021-10-12 01:59:58 +0000

    create model

--- src/test.py
+++ src/test.py
@@ -11,10 +11,47 @@ from tensorflow.keras.models import Sequential
 crypto_currency = 'SHIB'
 against_currency = 'USD'
 
-start = dt.datetime(2021,10,1)
+start = dt.datetime(2021,7,1)
 end = dt.datetime.now()
 
 data = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo', start, end)
 
 # Prepare Data
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
+
+prediction_days = 60
+
+x_train, y_train = [], []
+
+for x in range(prediction_days, len(scaled_data)):
+  x_train.append(scaled_data[x-prediction_days: x, 0])
+  y_train.append(scaled_data[x, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+# Create Neural Neetwork
+model = Sequential()
+
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(Dropout(0, 2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0, 2))
+model.add(LSTM(units=50))
+model.add(Dropout(0, 2))
+model.Dense(unit=1)
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train,y_train, epochs=25, batch_size=32)
+
+# Training Model
+test_start = dt.datetime(2021,7,1)
+test_end = dt.datetime.now()
+
+test_data = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo', test_start, test_end)
+actual_prices = test_data['Close'].values
+
+total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+
 
